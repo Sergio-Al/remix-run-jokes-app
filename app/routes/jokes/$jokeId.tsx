0b3aa1588c3d69a7d5f7a73649e63f6ebe5f2d1a
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-12-16 00:21:17 -0400

    Optimistic UI
    Now that we are using JavaScript we can use OptimisticUI to have Progressive enhancement to make our site even better.
    - We created our Joke as Reusable component.
    - Now we can use our site with enhanced features with slow networks

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,16 @@
 import { LoaderFunction, ActionFunction, MetaFunction } from "remix";
-import { Link, Form, useLoaderData, useParams, useCatch, redirect } from "remix";
+import {
+  Link,
+  Form,
+  useLoaderData,
+  useParams,
+  useCatch,
+  redirect,
+} from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
+import { JokeDisplay } from "~/components/joke";
 
 export const meta: MetaFunction = ({
   data,
@@ -59,21 +67,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      {data.isOwner ? (
-        <Form method="post">
-          <input type="hidden" name="_method" value="delete" />
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function CatchBoundary() {
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,5 +1,6 @@
-import type { ActionFunction, LoaderFunction } from "remix";
+import { ActionFunction, LoaderFunction, useTransition } from "remix";
 import { useActionData, redirect, json, useCatch, Link, Form } from "remix";
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
 
@@ -69,6 +70,26 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function NewJokeRoute() {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
+
+  if (transition.submission) {
+    const name = transition.submission.formData.get("name");
+    const content = transition.submission.formData.get("content");
+    if (
+      typeof name === "string" &&
+      typeof content === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          joke={{ name, content }}
+          isOwner={true}
+          canDelete={false}
+        />
+      );
+    }
+  }
 
   return (
     <div>
